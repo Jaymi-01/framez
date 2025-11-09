afe3ffe94daf89845cd73eb4697941be4dd35858
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-09 15:36:43 +0100

    Add Firebase configuration file

---
+++ src/api/firebase.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDvEdnCw5K7mmJwC6GNje8-b1syHwge-5M",
+  authDomain: "framez-app-f09c7.firebaseapp.com",
+  projectId: "framez-app-f09c7",
+  storageBucket: "framez-app-f09c7.firebasestorage.app",
+  messagingSenderId: "305120753170",
+  appId: "1:305120753170:web:6ee625f0836e756575cacb",
+  measurementId: "G-V2H1WB7T8Q"
+};
